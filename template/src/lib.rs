da7f04bda95d4080619ebf9e11aecc68d48e653b
Ben Sully <ben.sully@yougov.com>
2018-12-03 16:05:45 +0000

    Add simple template directory

---
+++ template/src/lib.rs
@@ -0,0 +1,25 @@
+pub fn part1(input: &[&str]) -> u64 {
+    unimplemented!()
+}
+
+pub fn part2(input: &[&str]) -> String {
+    unimplemented!()
+}
+
+#[cfg(test)]
+mod tests {
+
+    use super::{part1, part2};
+
+    #[test]
+    fn test_part1() {
+        let input = &[];
+        assert_eq!(part1(input), _);
+    }
+
+    #[test]
+    fn test_part2() {
+        let input = &[];
+        assert_eq!(part2(input), _);
+    }
+}
